42ffbbd0d712e2e8a6c50f24cbf61feb35262842
Mr Lii <leetamparega@gmail.com>
2025-06-04 16:49:37 +0000

    config: Create based and test all application works

---
+++ src/test/java/lii/buildmaster/projecttracker/ProjecttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lii.buildmaster.projecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
