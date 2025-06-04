c1b1d6681388b628c53c59289b3a260204194a18
Mr Lii <leetamparega@gmail.com>
2025-06-04 23:08:05 +0000

    docs: add ProjectStatus Attributes for enums

--- src/main/java/lii/buildmaster/projecttracker/model/enums/ProjectStatus.java
+++ src/main/java/lii/buildmaster/projecttracker/model/enums/ProjectStatus.java
@@ -1,4 +1,24 @@
 package lii.buildmaster.projecttracker.model.enums;
 
 public enum ProjectStatus {
+    PLANNING("Planning"),
+    IN_PROGRESS("In Progress"),
+    ON_HOLD("On Hold"),
+    COMPLETED("Completed"),
+    CANCELLED("Cancelled");
+
+    private final String displayName;
+
+    ProjectStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
