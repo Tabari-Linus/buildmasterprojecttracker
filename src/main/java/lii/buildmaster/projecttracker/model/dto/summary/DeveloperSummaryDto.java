8f0b2ba2f7be9b0830819a2584788f20803169d8
Mr Lii <leetamparega@gmail.com>
2025-06-05 13:52:27 +0000

    docs: create DeveloperSummaryDto to present a count of task assign to develop and grouped by status response to the endpoint

--- src/main/java/lii/buildmaster/projecttracker/model/dto/summary/DeveloperSummaryDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/summary/DeveloperSummaryDto.java
@@ -1,4 +1,16 @@
 package lii.buildmaster.projecttracker.model.dto.summary;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeveloperSummaryDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private long activeTaskCount;
 }
