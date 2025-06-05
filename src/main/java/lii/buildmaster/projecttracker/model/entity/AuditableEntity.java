9ffcd120a9d43428b181f01e7c2a2adcccc94406
Mr Lii <leetamparega@gmail.com>
2025-06-05 01:19:46 +0000

    docs: create Auditable to handle creation and update

--- src/main/java/lii/buildmaster/projecttracker/model/entity/AuditableEntity.java
+++ src/main/java/lii/buildmaster/projecttracker/model/entity/AuditableEntity.java
@@ -1,4 +1,25 @@
 package lii.buildmaster.projecttracker.model.entity;
 
-public class AuditableEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class AuditableEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
