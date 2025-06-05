93c5f2f918f36193fc2a98fb7f236786ae0f9985
Mr Lii <leetamparega@gmail.com>
2025-06-05 13:46:35 +0000

    docs: add DeveloperRequestdto for handling client request

--- src/main/java/lii/buildmaster/projecttracker/model/dto/request/DeveloperRequestDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/request/DeveloperRequestDto.java
@@ -1,4 +1,26 @@
-package lii.buildmaster.projecttracker.model.dto;
+package lii.buildmaster.projecttracker.model.dto.request;
 
-public class DeveloperDto {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeveloperRequestDto {
+
+    @NotBlank(message = "Developer name is required")
+    @Size(max = 100, message = "Name must not exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 150, message = "Email must not exceed 150 characters")
+    private String email;
+
+    @Size(max = 500, message = "Skills must not exceed 500 characters")
+    private String skills;
 }
