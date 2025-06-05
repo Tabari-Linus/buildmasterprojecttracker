b2fb5d2e0bb1711f8e6207c54dc5707b7d71f075
Mr Lii <leetamparega@gmail.com>
2025-06-05 01:40:29 +0000

    docs: Implemte TaskREspository

---
+++ src/main/java/lii/buildmaster/projecttracker/repository/jpa/TaskRepository.java
@@ -0,0 +1,58 @@
+package lii.buildmaster.projecttracker.repository.jpa;
+
+import lii.buildmaster.projecttracker.model.entity.Task;
+import lii.buildmaster.projecttracker.model.enums.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByProjectId(Long projectId);
+
+    List<Task> findByDeveloperId(Long developerId);
+
+    List<Task> findByDeveloperIsNull();
+
+    List<Task> findByStatus(TaskStatus status);
+
+    List<Task> findByProjectIdAndStatus(Long projectId, TaskStatus status);
+
+    List<Task> findByDeveloperIdAndStatus(Long developerId, TaskStatus status);
+
+    @Query("SELECT t FROM Task t WHERE t.dueDate < :currentTime AND t.status != 'DONE'")
+    List<Task> findOverdueTasks(@Param("currentTime") LocalDateTime currentTime);
+
+    @Query("SELECT t FROM Task t WHERE t.dueDate BETWEEN :startTime AND :endTime AND t.status != 'DONE'")
+    List<Task> findTasksDueWithinDays(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+
+    long countByStatus(TaskStatus status);
+
+    long countByProjectId(Long projectId);
+
+    long countByDeveloperId(Long developerId);
+
+    @Query("SELECT t.developer.id, t.developer.name, COUNT(t) as taskCount " +
+            "FROM Task t WHERE t.developer IS NOT NULL " +
+            "GROUP BY t.developer.id, t.developer.name " +
+            "ORDER BY taskCount DESC")
+    List<Object[]> findTopDevelopersWithMostTasks(Pageable pageable);
+
+    @Query("SELECT t.status, COUNT(t) FROM Task t GROUP BY t.status")
+    List<Object[]> getTaskCountsByStatus();
+
+    @Query("SELECT p FROM Project p WHERE p.id NOT IN (SELECT DISTINCT t.project.id FROM Task t)")
+    List<Object[]> findProjectsWithoutTasks();
+
+    @Query("SELECT t FROM Task t WHERE t.project.deadline < :currentTime AND t.status != 'DONE'")
+    List<Task> findTasksInOverdueProjects(@Param("currentTime") LocalDateTime currentTime);
+}
