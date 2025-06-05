fe67f981129823099e429b010a8d0c2071f7991b
Mr Lii <leetamparega@gmail.com>
2025-06-05 01:17:26 +0000

    docs: Create taskstatus enumerations

--- src/main/java/lii/buildmaster/projecttracker/model/enums/TaskStatus.java
+++ src/main/java/lii/buildmaster/projecttracker/model/enums/TaskStatus.java
@@ -1,4 +1,24 @@
 package lii.buildmaster.projecttracker.model.enums;
 
 public enum TaskStatus {
+    TODO("To Do"),
+    IN_PROGRESS("In Progress"),
+    IN_REVIEW("In Review"),
+    DONE("Done"),
+    BLOCKED("Blocked");
+
+    private final String displayName;
+
+    TaskStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
