5f045fe0fe5ccd0181cd9e81ccb909649a8a569f
Mr Lii <leetamparega@gmail.com>
2025-06-05 13:47:58 +0000

    docs: create ProjectResponseDto for handling response to the endpoint

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/ProjectResponseDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/ProjectResponseDto.java
@@ -1,4 +1,32 @@
 package lii.buildmaster.projecttracker.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lii.buildmaster.projecttracker.model.enums.ProjectStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectResponseDto {
+
+    private Long id;
+    private String name;
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime deadline;
+
+    private ProjectStatus status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    private long taskCount;
 }
