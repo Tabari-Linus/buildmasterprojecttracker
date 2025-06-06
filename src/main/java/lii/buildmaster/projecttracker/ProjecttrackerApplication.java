f11e0df709eeae50efe64c084fea6815fba111c6
Mr Lii <leetamparega@gmail.com>
2025-06-06 13:02:08 +0000

    docs: add EnableAspectJAutoProxy annotation to the ProjectMAin class

--- src/main/java/lii/buildmaster/projecttracker/ProjecttrackerApplication.java
+++ src/main/java/lii/buildmaster/projecttracker/ProjecttrackerApplication.java
@@ -3,11 +3,15 @@ package lii.buildmaster.projecttracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
+@EnableAspectJAutoProxy
 public class ProjecttrackerApplication {
 
     public static void main(String[] args) {
