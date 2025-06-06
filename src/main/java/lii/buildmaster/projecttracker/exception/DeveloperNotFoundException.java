782f23ba333040881888d9fef610f59371f284e7
Mr Lii <leetamparega@gmail.com>
2025-06-06 19:00:20 +0000

    docs: extended runtimeexception and add controllers

--- src/main/java/lii/buildmaster/projecttracker/exception/DeveloperNotFoundException.java
+++ src/main/java/lii/buildmaster/projecttracker/exception/DeveloperNotFoundException.java
@@ -1,4 +1,11 @@
 package lii.buildmaster.projecttracker.exception;
 
-public class DeveloperNotFoundException {
+public class DeveloperNotFoundException extends RuntimeException{
+
+    private final long developerId;
+
+    public DeveloperNotFoundException(long developerId) {
+        super("Develops with ID " + developerId + " not found.");
+        this.developerId = developerId;
+    }
 }
--- src/main/java/lii/buildmaster/projecttracker/exception/ProjectNotFoundException.java
+++ src/main/java/lii/buildmaster/projecttracker/exception/ProjectNotFoundException.java
@@ -1,4 +1,14 @@
 package lii.buildmaster.projecttracker.exception;
 
-public class ProjectNotFoundException {
+import lombok.Getter;
+
+@Getter
+public class ProjectNotFoundException extends RuntimeException{
+
+    private final long projectId;
+
+    public ProjectNotFoundException(long projectId) {
+        super("Project with ID " + projectId + " not found.");
+        this.projectId = projectId;
+    }
 }
