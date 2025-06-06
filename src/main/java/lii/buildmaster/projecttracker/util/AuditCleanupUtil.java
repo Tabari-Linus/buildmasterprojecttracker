fca8a7be6e90ddfee40ff35a6bd27f237f2672a0
Mr Lii <leetamparega@gmail.com>
2025-06-06 11:56:13 +0000

    docs: to delete logs as we develop

--- src/main/java/lii/buildmaster/projecttracker/util/AuditCleanupUtil.java
+++ src/main/java/lii/buildmaster/projecttracker/util/AuditCleanupUtil.java
@@ -1,4 +1,24 @@
 package lii.buildmaster.projecttracker.util;
 
+import lii.buildmaster.projecttracker.repository.mongo.AuditLogRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuditCleanupUtil {
+
+    private final AuditLogRepository auditLogRepository;
+
+    public AuditCleanupUtil(AuditLogRepository auditLogRepository) {
+        this.auditLogRepository = auditLogRepository;
+    }
+
+    public void clearAllAuditLogs() {
+        long count = auditLogRepository.count();
+        auditLogRepository.deleteAll();
+        System.out.println("Cleared " + count + " audit logs from MongoDB");
+    }
+
+    public long getAuditLogCount() {
+        return auditLogRepository.count();
+    }
 }
