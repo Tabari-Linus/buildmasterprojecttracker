91475d17379e39a4a23a8908bad6234a684cb7b3
Mr Lii <leetamparega@gmail.com>
2025-06-06 11:00:06 +0000

    docs: create Audit actionType and Entitytype Enums

--- src/main/java/lii/buildmaster/projecttracker/model/enums/ActionType.java
+++ src/main/java/lii/buildmaster/projecttracker/model/enums/ActionType.java
@@ -1,4 +1,29 @@
 package lii.buildmaster.projecttracker.model.enums;
 
-public class ActionType {
+public enum ActionType {
+    CREATE("Create"),
+    UPDATE("Update"),
+    DELETE("Delete"),
+    ASSIGN("Assign"),
+    UNASSIGN("Unassign"),
+    STATUS_CHANGE("Status Change"),
+    LOGIN("Login"),
+    LOGOUT("Logout"),
+    BULK_UPDATE("Bulk Update"),
+    BULK_DELETE("Bulk Delete");
+
+    private final String displayName;
+
+    ActionType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
--- src/main/java/lii/buildmaster/projecttracker/model/enums/EntityType.java
+++ src/main/java/lii/buildmaster/projecttracker/model/enums/EntityType.java
@@ -1,4 +1,24 @@
 package lii.buildmaster.projecttracker.model.enums;
 
-public class EntityType {
+public enum EntityType {
+    PROJECT("Project"),
+    DEVELOPER("Developer"),
+    TASK("Task"),
+    USER("User"),
+    SYSTEM("System");
+
+    private final String displayName;
+
+    EntityType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
