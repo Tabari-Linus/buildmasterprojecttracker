c31f8964314b2ee2833d73b1668eeb4fe976390d
Mr Lii <leetamparega@gmail.com>
2025-06-06 22:14:14 +0000

    docs: rename to ErrorResponseDto

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/ErrorResponseDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/ErrorResponseDto.java
@@ -1,11 +1,11 @@
-package lii.buildmaster.projecttracker.model.dto.request;
+package lii.buildmaster.projecttracker.model.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class ApiErrorResponse {
+public class ErrorResponseDto {
     private String message;
     private String status;
     private int statusCode;
