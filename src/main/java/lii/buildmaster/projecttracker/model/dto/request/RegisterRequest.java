a37573871d9ce13340bf61cb83e27cb37d50d895
Mr Lii <leetamparega@gmail.com>
2025-06-11 17:01:29 +0000

    docs: Add fields for RegisterRequestDto

--- src/main/java/lii/buildmaster/projecttracker/model/dto/request/RegisterRequest.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/request/RegisterRequest.java
@@ -1,4 +1,36 @@
 package lii.buildmaster.projecttracker.model.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Use a valid email address")
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
+    private String password;
+
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank(message = "Role is required")
+    @Size(max = 20, message = "Role must not exceed 20 characters")
+    private String role;
 }
