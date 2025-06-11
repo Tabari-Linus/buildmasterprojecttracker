cc8d58c20633db7be8bc17c19ff8bcac7f569c4d
Mr Lii <leetamparega@gmail.com>
2025-06-11 17:41:11 +0000

    docs: add methods to findByname ( rolename) and if roleexist by name

--- src/main/java/lii/buildmaster/projecttracker/repository/jpa/RoleRepository.java
+++ src/main/java/lii/buildmaster/projecttracker/repository/jpa/RoleRepository.java
@@ -1,4 +1,13 @@
 package lii.buildmaster.projecttracker.repository.jpa;
 
-public interface RoleRepository {
+import lii.buildmaster.projecttracker.model.entity.Role;
+import lii.buildmaster.projecttracker.model.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+    boolean existsByName(RoleName name);
 }
