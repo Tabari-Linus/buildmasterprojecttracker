9e74bb9f67d1a1dc96d484f43d264d58b0952372
Mr Lii <leetamparega@gmail.com>
2025-06-11 17:20:24 +0000

    docs: add fields accessToken, refreshToken, tokenType

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/TokenRefreshResponse.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/TokenRefreshResponse.java
@@ -1,4 +1,7 @@
 package lii.buildmaster.projecttracker.model.dto.response;
 
 public class TokenRefreshResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
 }
