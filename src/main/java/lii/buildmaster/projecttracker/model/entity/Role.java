c203d3472f7296d5f4a639de5fa51270cb2501dd
Mr Lii <leetamparega@gmail.com>
2025-06-11 16:22:50 +0000

    docs: add attribute for role ( id, name, description) and a relationship to the user class

--- src/main/java/lii/buildmaster/projecttracker/model/entity/Role.java
+++ src/main/java/lii/buildmaster/projecttracker/model/entity/Role.java
@@ -1,4 +1,34 @@
 package lii.buildmaster.projecttracker.model.entity;
 
+import jakarta.persistence.*;
+import lii.buildmaster.projecttracker.model.enums.RoleName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, unique = true, nullable = false)
+    private RoleName name;
+
+    @Column(length = 100)
+    private String description;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 }
