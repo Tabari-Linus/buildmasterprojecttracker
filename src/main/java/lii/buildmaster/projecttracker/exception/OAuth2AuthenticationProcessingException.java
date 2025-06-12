3a217638fef06c67b1c777edf5d4d9b9e49184a9
Mr Lii <leetamparega@gmail.com>
2025-06-12 13:25:17 +0000

    docs: extend RuntimeException

--- src/main/java/lii/buildmaster/projecttracker/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/lii/buildmaster/projecttracker/exception/OAuth2AuthenticationProcessingException.java
@@ -1,4 +1,7 @@
 package lii.buildmaster.projecttracker.exception;
 
-public class OAuth2AuthenticationProcessingException {
+public class OAuth2AuthenticationProcessingException extends RuntimeException {
+    public OAuth2AuthenticationProcessingException(String s) {
+        super(s);
+    }
 }
