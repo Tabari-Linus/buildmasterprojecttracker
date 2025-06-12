2b97a8d781a7152172fb30a7b37861cf7653dcf7
Mr Lii <leetamparega@gmail.com>
2025-06-12 12:19:56 +0000

    docs: add Data, Builder, AllArgsConstractor and NoArgsConstractor annotation

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/TokenRefreshResponseDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/TokenRefreshResponseDto.java
@@ -1,5 +1,14 @@
 package lii.buildmaster.projecttracker.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenRefreshResponseDto {
     private String accessToken;
     private String refreshToken;
