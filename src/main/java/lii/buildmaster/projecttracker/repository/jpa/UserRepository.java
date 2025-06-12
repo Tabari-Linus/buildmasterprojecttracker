809a5b4fce8b529d58bac54dc7f71c4f471217d4
Mr Lii <leetamparega@gmail.com>
2025-06-12 17:51:38 +0000

    docs: add Transaction annotation to updateLastLogin

--- src/main/java/lii/buildmaster/projecttracker/repository/jpa/UserRepository.java
+++ src/main/java/lii/buildmaster/projecttracker/repository/jpa/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,6 +33,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailWithRoles(@Param("email") String email);
 
     @Modifying
+    @Transactional
     @Query("UPDATE User u SET u.lastLoginAt = :lastLogin WHERE u.id = :userId")
     void updateLastLogin(@Param("userId") Long userId, @Param("lastLogin") LocalDateTime lastLogin);
 
