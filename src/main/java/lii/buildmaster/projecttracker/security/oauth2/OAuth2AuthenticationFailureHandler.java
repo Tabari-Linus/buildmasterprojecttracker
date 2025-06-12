6a27199c80bd2f02ac7133bc6417584ceaeb8224
Mr Lii <leetamparega@gmail.com>
2025-06-12 20:42:50 +0000

    docs: add Method onAuthenticationFailure to handle Oauth2 failure

--- src/main/java/lii/buildmaster/projecttracker/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/lii/buildmaster/projecttracker/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,4 +1,47 @@
 package lii.buildmaster.projecttracker.security.oauth2;
 
-public class OAuth2AuthenticationFailureHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lii.buildmaster.projecttracker.util.CookieUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+    @Value("${app.oauth2.default-failure-url}")
+    private String defaultFailureUrl;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl = CookieUtils.getCookie(request,
+                        HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse(defaultFailureUrl);
+
+        // Safely encode error message
+        String encodedErrorMessage = URLEncoder.encode(exception.getLocalizedMessage(), StandardCharsets.UTF_8);
+
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", encodedErrorMessage)
+                .build().toUriString();
+
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
 }
