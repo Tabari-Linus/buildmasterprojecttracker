f3d249063888ae76b3d6ddf114379e38d0d40d10
Mr Lii <leetamparega@gmail.com>
2025-06-12 20:41:06 +0000

    docs: method to return Oauth success

--- src/main/java/lii/buildmaster/projecttracker/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/lii/buildmaster/projecttracker/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,4 +1,131 @@
 package lii.buildmaster.projecttracker.security.oauth2;
 
-public class OAuth2AuthenticationSuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lii.buildmaster.projecttracker.exception.BadRequestException;
+import lii.buildmaster.projecttracker.model.entity.User;
+import lii.buildmaster.projecttracker.repository.jpa.UserRepository;
+import lii.buildmaster.projecttracker.util.CookieUtils;
+import lii.buildmaster.projecttracker.util.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtils jwtUtils;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final UserRepository userRepository;
+
+    @Value("${app.oauth2.default-success-url}")
+    private String defaultSuccessUrl;
+
+    @Value("${app.oauth2.authorized-redirect-uris}")
+    private String[] authorizedRedirectUris;
+
+    @Override
+    @Transactional
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+
+        if (response.isCommitted()) {
+            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            return;
+        }
+
+        clearAuthenticationAttributes(request, response);
+
+        // Update last login time
+        updateLastLogin(authentication);
+
+        // Log successful OAuth2 authentication
+        if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            log.info("OAuth2 authentication successful for user: {} (ID: {})",
+                    oAuth2User.getEmail(), oAuth2User.getId());
+        }
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request,
+                        HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+        }
+
+        String targetUrl = redirectUri.orElse(defaultSuccessUrl);
+
+        // Generate JWT token
+        String token = jwtUtils.generateJwtToken(authentication);
+        String refreshToken = jwtUtils.generateRefreshToken(authentication.getName());
+
+        // Get user info
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", token)
+                .queryParam("refreshToken", refreshToken)
+                .queryParam("userId", oAuth2User.getId())
+                .build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+
+        for (String authorizedRedirectUri : authorizedRedirectUris) {
+            URI authorizedURI = URI.create(authorizedRedirectUri);
+            if (authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                    && authorizedURI.getPort() == clientRedirectUri.getPort()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateLastLogin(Authentication authentication) {
+        try {
+            if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+                CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+                // Alternative approach: Update the user entity directly
+                Optional<User> userOptional = userRepository.findById(oAuth2User.getId());
+                if (userOptional.isPresent()) {
+                    User user = userOptional.get();
+                    user.setLastLoginAt(LocalDateTime.now());
+                    userRepository.save(user);
+                    log.debug("Updated last login time for user: {}", user.getEmail());
+                }
+            }
+        } catch (Exception e) {
+            log.error("Failed to update last login time", e);
+            // Don't fail the authentication process due to this error
+
+        }
+    }
 }
