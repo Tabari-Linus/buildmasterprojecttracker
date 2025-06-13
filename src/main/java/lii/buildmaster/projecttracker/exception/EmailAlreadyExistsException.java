a2dfbd83285ba92059b1b870f52e9c4f2a5eb6c9
Mr Lii <leetamparega@gmail.com>
2025-06-13 21:10:02 +0000

    docs: Extend ExistsException

--- src/main/java/lii/buildmaster/projecttracker/exception/EmailAlreadyExistsException.java
+++ src/main/java/lii/buildmaster/projecttracker/exception/EmailAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package lii.buildmaster.projecttracker.exception;
 
-public class EmailAlreadyExistsException {
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String s) {
+        super(s);
+    }
 }
