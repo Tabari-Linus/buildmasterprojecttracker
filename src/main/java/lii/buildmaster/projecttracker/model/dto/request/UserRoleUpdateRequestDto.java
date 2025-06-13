7077d5bccca3ecd648901445ad1796fc0b7b4b9d
Mr Lii <leetamparega@gmail.com>
2025-06-13 00:09:53 +0000

    docs: ass roles

--- src/main/java/lii/buildmaster/projecttracker/model/dto/request/UserRoleUpdateRequestDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/request/UserRoleUpdateRequestDto.java
@@ -1,4 +1,18 @@
 package lii.buildmaster.projecttracker.model.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRoleUpdateRequestDto {
+    @NotEmpty(message = "At least one role must be specified")
+    private Set<String> roles;
 }
