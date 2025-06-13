586e8055ffdbb2cc0aea0009fd52a58a8bc0510f
Mr Lii <leetamparega@gmail.com>
2025-06-13 00:13:20 +0000

    docs: add fields

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/UserResponseDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/UserResponseDto.java
@@ -1,4 +1,28 @@
 package lii.buildmaster.projecttracker.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Set<String> roles;
+    private String provider;
+    private boolean enabled;
+    private boolean accountNonLocked;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLoginAt;
+    private Long developerId;
 }
