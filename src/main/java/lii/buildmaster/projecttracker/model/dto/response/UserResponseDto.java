a76fe98c6fee3a488406f2f94aed70802289671a
Mr Lii <leetamparega@gmail.com>
2025-06-13 09:06:41 +0000

    docs: change rolse set type to Role and add Custom constractor

--- src/main/java/lii/buildmaster/projecttracker/model/dto/response/UserResponseDto.java
+++ src/main/java/lii/buildmaster/projecttracker/model/dto/response/UserResponseDto.java
@@ -1,5 +1,9 @@
 package lii.buildmaster.projecttracker.model.dto.response;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lii.buildmaster.projecttracker.model.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,11 +22,19 @@ public class UserResponseDto {
     private String email;
     private String firstName;
     private String lastName;
-    private Set<String> roles;
+    private Set<Role> roles;
     private String provider;
     private boolean enabled;
     private boolean accountNonLocked;
     private LocalDateTime createdAt;
     private LocalDateTime lastLoginAt;
     private Long developerId;
+
+    public UserResponseDto(Long id, @NotBlank @Size(max = 50) String username, @NotBlank @Size(max = 100) @Email String email, boolean enabled, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
 }
