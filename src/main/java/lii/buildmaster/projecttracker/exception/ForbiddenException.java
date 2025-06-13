a21000c4fa74a247550f39f4ae202d6be54759a5
Mr Lii <leetamparega@gmail.com>
2025-06-13 09:15:55 +0000

    docs: create ForbiddenException

---
+++ src/main/java/lii/buildmaster/projecttracker/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package lii.buildmaster.projecttracker.exception;
+
+public class ForbiddenException extends RuntimeException {
+  public ForbiddenException(String message) {
+    super(message);
+  }
+}
