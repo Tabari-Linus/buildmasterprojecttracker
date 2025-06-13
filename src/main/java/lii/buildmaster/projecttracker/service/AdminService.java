ffc185708aa6c97d4db362534ef6a6c1da5e52f3
Mr Lii <leetamparega@gmail.com>
2025-06-13 09:11:52 +0000

    docs: add abstract methods for getting all users, getAllUsers, updateUserById,

--- src/main/java/lii/buildmaster/projecttracker/service/AdminService.java
+++ src/main/java/lii/buildmaster/projecttracker/service/AdminService.java
@@ -1,4 +1,36 @@
 package lii.buildmaster.projecttracker.service;
 
-public class AdminService {
+import lii.buildmaster.projecttracker.model.dto.request.UserRoleUpdateRequestDto;
+import lii.buildmaster.projecttracker.model.dto.response.UserResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+public interface AdminService {
+
+    Page<UserResponseDto> getAllUsers(Pageable pageable);
+
+    UserResponseDto getUserById(@PathVariable Long id);
+
+    UserResponseDto updateUserRoles(@PathVariable Long id, UserRoleUpdateRequestDto roleUpdateRequest);
+
+    UserResponseDto updateUserStatus(@PathVariable Long id, boolean enabled);
+
+    void deleteUser(@PathVariable Long id);
+
+    Page<UserResponseDto> getUsersByRole(@PathVariable String roleName, Pageable pageable);
+
+    Map<String, Object> getSystemStatistics();
+
+    Page<? > getAuditLogs(String entityType, String actionType, Pageable pageable);
+
+    UserResponseDto approveContractor(
+            @PathVariable Long id,
+            @RequestParam String newRole);
+
+    String resetUserPassword(@PathVariable Long id);
+
 }
