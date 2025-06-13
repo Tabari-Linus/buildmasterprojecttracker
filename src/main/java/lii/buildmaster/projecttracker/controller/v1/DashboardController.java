2c9a32cc8b8df92ded4bfe5d63c776514615a130
Mr Lii <leetamparega@gmail.com>
2025-06-13 18:35:52 +0000

    docs: add DashBoardController

--- src/main/java/lii/buildmaster/projecttracker/controller/v1/DashboardController.java
+++ src/main/java/lii/buildmaster/projecttracker/controller/v1/DashboardController.java
@@ -1,4 +1,16 @@
 package lii.buildmaster.projecttracker.controller.v1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
+
+    @GetMapping
+    public String viewDashboard() {
+        return "dashboardPage"; // maps to a file like `dashboardPage.html` or `dashboardPage.jsp`
+    }
 }
+
