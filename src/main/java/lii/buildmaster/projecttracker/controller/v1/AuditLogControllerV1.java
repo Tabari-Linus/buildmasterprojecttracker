afc00fafcfacefff36e6e744249cdc51776b4108
Mr Lii <leetamparega@gmail.com>
2025-06-14 01:30:56 +0000

    docs: update Preauth to ADMIN only

--- src/main/java/lii/buildmaster/projecttracker/controller/v1/AuditLogControllerV1.java
+++ src/main/java/lii/buildmaster/projecttracker/controller/v1/AuditLogControllerV1.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -24,8 +25,8 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/audit-logs")
-@CrossOrigin(origins = "*")
 @Tag(name = "Audit Logs", description = "AUdit Logger for tacking user activities and changes to entities")
+@PreAuthorize("hasRole('ROLE_ADMIN') ")
 public class AuditLogControllerV1 {
 
     private final AuditLogService auditLogService;
--- src/main/java/lii/buildmaster/projecttracker/controller/v1/AuditManagementControllerV1.java
+++ src/main/java/lii/buildmaster/projecttracker/controller/v1/AuditManagementControllerV1.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lii.buildmaster.projecttracker.util.AuditCleanupUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/audit-management")
 @Tag(name = "Audit Management", description = "Administrative operations for audit log management")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AuditManagementControllerV1 {
 
     private final AuditCleanupUtil auditCleanupUtil;
