eca1fc7aa9d883b3d3c379f9aff652f440985612
Mr Lii <leetamparega@gmail.com>
2025-06-28 23:18:28 +0000

    docs: remove commit

--- src/main/java/lii/buildmaster/projecttracker/repository/jpa/TaskRepository.java
+++ src/main/java/lii/buildmaster/projecttracker/repository/jpa/TaskRepository.java
@@ -2,24 +2,26 @@ package lii.buildmaster.projecttracker.repository.jpa;
 
 import lii.buildmaster.projecttracker.model.entity.Task;
 import lii.buildmaster.projecttracker.model.enums.TaskStatus;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-// import org.springframework.data.jpa.repository.EntityGraph; // Temporarily remove this import
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional; // IMPORTANT: Add this import if not present
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByProjectId(Long projectId);
+
     List<Task> findByDeveloperId(Long developerId);
+
     List<Task> findByDeveloperIsNull();
+
     List<Task> findByStatus(TaskStatus status);
 
     @Query("SELECT t FROM Task t WHERE t.dueDate < :currentTime AND t.status != 'DONE'")
@@ -31,7 +33,9 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByTitleContainingIgnoreCase(String title);
 
     long countByStatus(TaskStatus status);
+
     long countByProjectId(Long projectId);
+
     long countByDeveloperId(Long developerId);
 
     @Query("SELECT t.developer.id, t.developer.name, COUNT(t) as taskCount " +
